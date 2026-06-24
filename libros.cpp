bff353f1b89ebf04839686d7ccb4071c98827953
Giovannym27 <gmamanim@unjbg.edu.pe>
2026-06-24 10:21:09 -0500

    Avance de libros

---
+++ libros.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Libro{
+    string titulo;
+    string autor;
+    string editorial;
+    int anioPublicacion;
+};
+
+int main (){
+    int n;
+    Libro libros[100];
+    cout<<"Ingrese la cantidad de libros: ";
+    cin>> n;
+    cin.ignore();
+
+    for (int i=0; i<n; i++){
+        cout<<"\nLibro"<< i+1<<endl;
+        cout<<"Titulo: ";
+    }
+}
