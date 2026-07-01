7bbf9e26f9e8fde631b01707aae2fca777a4e00d
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-06-30 22:50:08 -0500

    Primer intento de propuesto1

---
+++ propuesto#1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Empleado{
+    string nombre;
+    char sexo;
+    float sueldo;
+};
+
+int main() {
+
+    int n;
+
+    cout<<"Cantidad de empleados: ";
+    cin>>n;
+
+    Empleado emp[n];
+
+    
+}
