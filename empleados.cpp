e713ac61a93220d6deb2b42b97ecd55de2792ba8
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-07-01 10:31:00 -0500

    Primer commit

---
+++ empleados.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using name space std;
+
+struct 	Empleado{
+	int numero;
+	string nombre;
+	int ventas;
+	float salario; 
+};
+
+int main(){
+	int n;
+	Empleado empleados[500]
+	cout<<"Ingrese la cantidad de empleados"
+	cin>>n;
+	cin.ignore();
+	
+	for(int i=0; i<n; i++){
+	cout<<"\nEmpleado"<<i+1<<endl;
+	cout<<"Numero: ";
+	cin>>empleados[i].numero;
+	cout<<"Nombre: ";
+	getline(cin, empleados[i].nombre);
+	
+	for (int j=0; j<=12; j++){
+			cout<<"+"
+		}
+	} 
+	
+	
+	
+	
+}
