b664b76d65701793a790f8cd3c439a68a630e908
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-06-30 23:05:00 -0500

    Integracion de las estructuras for y los mensajes al usuario

--- propuesto2.cpp
+++ propuesto2.cpp
@@ -12,5 +12,45 @@ int main(){
 
     int n;
 
+    cout<<"Cantidad de personas: ";
+    cin>>n;
 
+    Persona p[n];
+
+    int mayores50=0;
+    int sumaEdades=0;
+
+    for(int i=0;i<n;i++){
+
+        cout<<"\nPersona "<<i+1<<endl;
+
+        cin.ignore();
+
+        cout<<"Nombre: ";
+        getline(cin,p[i].nombre);
+
+        cout<<"DNI: ";
+        getline(cin,p[i].dni);
+
+        cout<<"Edad: ";
+        cin>>p[i].edad;
+
+        sumaEdades+=p[i].edad;
+
+        if(p[i].edad>50)
+            mayores50++;
+    }
+
+    cout<<"\nPersonas mayores de 50: "<<mayores50<<endl;
+    cout<<"Promedio de edades: "<<(float)sumaEdades/n<<endl;
+
+    cout<<"\nListado de personas\n";
+
+    for(int i=0;i<n;i++){
+        cout<<"\nNombre: "<<p[i].nombre;
+        cout<<"\nDNI: "<<p[i].dni;
+        cout<<"\nEdad: "<<p[i].edad<<endl;
+    }
+
+    return 0;
 }
