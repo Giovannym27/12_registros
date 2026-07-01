1d6a53e4084dfb3f52af679340e17cfc43c1f639
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-06-30 23:12:04 -0500

    Primer intento de propuesto4

---
+++ propuesto4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Atleta{
+    string nombre;
+    string pais;
+    string disciplina;
+    int medallas;
+};
+
+int main(){
+
+    int n;
+
+    
+}
